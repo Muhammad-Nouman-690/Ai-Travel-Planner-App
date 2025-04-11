31acfdd3917a258b4cc997254ac0ba4e99431d1a
Muhammad-Nouman-690 <nomi690@hotmail.com>
2025-04-12 02:22:31 +0500

    home page done

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,17 +1,35 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import Entypo from '@expo/vector-icons/Entypo';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TabLayout() {
   return (
-    <View>
-      <Tabs screenOptions={{
-        headerShown:false
-      }}>
-        <Tabs.Screen name='mytrip' />
-        <Tabs.Screen name='discover' />
-        <Tabs.Screen name='profile' />
-      </Tabs>
-    </View>
+
+    <Tabs screenOptions={{
+      headerShown: false
+    }}>
+      <Tabs.Screen name='mytrip'
+        options={{
+          tabBarLabel: 'My Trip',
+          tabBarIcon: ({ color }) => <Entypo name="location" size={24} color="black" />
+        }}
+      />
+      <Tabs.Screen name='discover'
+      options={{
+        tabBarLabel: 'Discover',
+        tabBarIcon: ({ color }) => <FontAwesome name="globe" size={24} color="black" />
+      }}
+      />
+      <Tabs.Screen name='profile'
+      options={{
+        tabBarLabel: 'Profile',
+        tabBarIcon: ({ color }) => <Ionicons name="people" size={24} color="black" />
+      }}
+      />
+    </Tabs>
+
   )
 }
---
+++ components/MyTrips/StartNewTripCard.jsx
@@ -0,0 +1,51 @@
+import { View, Text, Touchable, TouchableOpacity } from 'react-native'
+import React from 'react'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Colors } from '../../app-example/constants/Colors';
+
+export default function StartNewTripCard() {
+    return (
+        <View style={{
+            padding: 20,
+            marginTop: 50,
+            display: 'flex',
+            alignItems: 'center',
+            gap: 25
+        }}>
+            <Ionicons name="location" size={30} color="black" />
+            <Text style={{
+                fontSize: 25,
+                fontFamily: 'outfit-bold',
+
+            }}>
+                No Trips Planned Yet
+            </Text>
+
+            <Text style={{
+                fontSize: 20,
+                fontFamily: 'outfit-medium',
+                textAlign:'center',
+                color:Colors.GRAY
+
+            }}>
+                Looks like its time to plan a new travel experience! Get Started below
+            </Text>
+            <TouchableOpacity style={{
+                    padding:15,
+                    backgroundColor:Colors.PRIMARY,
+                    borderRadius:15,
+                    paddingHorizontal:30
+                }}>
+                <Text 
+                style={{
+                    color:Colors.WHITE,
+                    fontFamily:'outfit-medium',
+                    fontSize:17
+                }}
+                >
+                    Start a new trip
+                </Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
